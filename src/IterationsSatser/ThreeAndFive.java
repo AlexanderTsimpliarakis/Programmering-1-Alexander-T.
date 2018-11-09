660d182282d5faa27d62f6472127452818b49b53
AlexanderTsimpliarakis <alexander.tsimpliarakis@elev.ga.ntig.se>
2018-11-09 10:41:01 +0100

    Iterationssats

---
+++ src/IterationsSatser/ThreeAndFive.java
@@ -0,0 +1,19 @@
+package IterationsSatser;
+
+public class ThreeAndFive {
+
+	public static void main(String[] args) {
+		
+		for(int i = 3; i <=100; i += 3) {
+			System.out.println(i);
+		}
+		for(int i = 5; i <=100; i += 5) {
+			System.out.println(i);
+		}
+		for(int i = 15; i <=100; i += 15) {
+			System.out.println(i);
+		}
+
+	}
+
+}
